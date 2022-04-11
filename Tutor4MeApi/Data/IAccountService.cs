bad6a22794ab299c2d665b6bad07531111aeff32
mothupir <mothupir@bbd.co.za>
2022-04-11 22:18:40 +0200

    updated databases

--- Tutor4MeApi/Data/IAccountService.cs
+++ Tutor4MeApi/Data/IAccountService.cs
@@ -4,7 +4,11 @@ namespace Tutor4MeApi.Data
 {
     public interface IAccountService
     {
+        // Tutor
         int CreateTutor(Tutor tutor);
+
+
+        // Student
         int CreateStudent(Student student);
     }
 }
